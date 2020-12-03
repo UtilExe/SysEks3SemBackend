8cc81e363f20a421ba00f8604871caf38250d65c
UtilExe <laymasterlolz@gmail.com>
2020-12-03 11:53:13 +0100

    Small adjustments for error cast

--- src/test/java/rest/SongResourceTest.java
+++ src/test/java/rest/SongResourceTest.java
@@ -137,7 +137,7 @@ public class SongResourceTest {
                 .body(jsonRequest)
                 .header("x-access-token", securityToken)
                 .when().post("/song/search").then()
-                .statusCode(204)
+                .statusCode(404)
                 .body("message", equalTo(messages.songNotFound));
     }
     
